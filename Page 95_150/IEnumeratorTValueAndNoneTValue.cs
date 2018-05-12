7888f15bfcea570d1b10cb6e6951e7910fba4c29
Leonardo Touma <leonardo.touma@capgemini.com>
2018-05-12 21:13:13 +0200

    added info from page 95-150

---
+++ Page 95_150/IEnumeratorTValueAndNoneTValue.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page_95_150
+{
+    class IEnumeratorTValueAndNoneTValue
+    {
+        /*
+         IEnumerator & IEnumerator<T>
+            .NET defined two base class libraries. There are non-generic and generic IEnumerator interfaces to define
+            the iteration of a collection.
+            IEnumerator
+            IEnumerator is non-generic interface defined in System.Collections namespace. It has methods and
+            properties that a collection implements to define its iteration.
+         */
+    }
+    public interface IEnumerator
+    {
+        //Gets value of current index of collection
+        object Current { get; }
+        //Move to the next index of the collection
+        bool MoveNext();
+        //Move to the initial position of index = -1
+        void Reset();
+    }
+    
+    class People : IEnumerable
+    {
+        Persons[] people;
+        int index = -1;
+
+        public void Add(Persons per)
+        {
+            if (++index < people.Length)
+            {
+                people[index] = per;
+            }
+        }
+        public People(int size)
+        {
+            people = new Persons[size];
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return new PersonEnum(people);
+        }
+    }
+    //Implement IEnumerator
+    public class PersonEnum : IEnumerator
+    {
+        Persons[] _people;
+        int index = -1;
+        public PersonEnum(Persons[] people)
+        {
+            _people = people;
+        }
+        //Check whether foreach can move to next iteration or not
+        public bool MoveNext()
+        {
+            return (++index < _people.Length);
+        }
+        //Reset the iteration
+        public void Reset()
+        {
+            index = -1;
+        }
+        //Get current value
+        public object Current
+        {
+            get
+            {
+                return _people[index];
+            }
+        }
+        class Persons
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                People people = new People(3);
+                people.Add(new Persons { Name = "Ali", Age = 22 });
+                people.Add(new Persons { Name = "Sundus", Age = 21 });
+                people.Add(new Persons { Name = "Hogi", Age = 12 });
+                foreach (var item in people)
+                {
+                    //Cast from object to Person
+                    Persons person = (Persons)item;
+                    Console.WriteLine("Name:{0} Age:{1}", person.Name, person.Age);
+                }
+            }
+        }
+    }
+}
--- Page 95_150/Interfaces_Frameworks.cs
+++ Page 95_150/Interfaces_Frameworks.cs
@@ -20,6 +20,7 @@ namespace Page_95_150
         Foreach loop only iterate over those types which implemented IEnumerable interface,
         i.e., ArrayList and Queue.
     */
+
     public interface IEnumerable
     {
         IEnumerator GetEnumerator();
@@ -65,5 +66,72 @@ namespace Page_95_150
             Sundus
             21
          */
+    }
+
+    /*
+     //
+     //
+     //
+     //
+     IEnumerable <T>
+     //
+     //
+     //
+     //
+     */
+    public interface IEnumerable<out T> : IEnumerable
+    {
+        IEnumerator<T> GetEnumerator();
+    }
+    class myList<T> : IEnumerable<T>
+    {
+        List<T> list = new List<T>();
+        //Get length of list<T>
+        public int Length
+        {
+            get { return list.Count; }
+        }
+        public void Add(T data)
+        {
+            list.Add(data);
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in list)
+            {
+                yield return item;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            //return IEnumerator<T> GetEnumerator()
+            return this.GetEnumerator();
+        }
+    }
+    partial class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myList<Person> people = new myList<Person>();
+            people.Add(new Person { Name = "Ali", Age = 22 });
+            people.Add(new Person { Name = "Sundus", Age = 21 });
+            people.Add(new Person { Name = "Hogi", Age = 12 });
+            Console.WriteLine("Total person: {0} \n", people.Length);
+            foreach (Person person in people)
+            {
+                Console.WriteLine("Name:{0} Age:{1}", person.Name, person.Age);
+            }
+            //Output
+            //
+            //Total Person: 3
+            //Name: Ali Age:22
+            //Name: Sundus Age:21
+            //Name: Hogi Age:12
+        }
     }
 }
