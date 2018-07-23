257a99c181fe23ae6e966c33194d61dc0f8a71c4
Leonardo Touma <leonardo.touma@capgemini.com>
2018-07-23 08:55:23 +0200

    added some about struct, tried using it as a viewmodel

---
+++ StructViewModel/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructViewModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student stud = new student();
+            Console.WriteLine($"Name: {stud.binder()}");
+            Console.Read();
+        }
+    }
+    class student
+    {
+        public PersonViewmodel person { get; set; }
+        public student()
+        {
+            person = new PersonViewmodel();
+        }
+        public string Name { get; set; } = "hej";
+        public string binder()
+        {
+            Name += person.Name;
+            return Name;
+        }
+    }
+    struct PersonViewmodel
+    {
+        public string Name { get; set; }
+    }
+}
